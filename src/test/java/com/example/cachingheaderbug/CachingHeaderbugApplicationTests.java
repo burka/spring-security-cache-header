0bb2487246eb97dbf40906083c28fd56f6bde183
Florian Burka <burka@brain-cool-schmidt.com>
2024-10-22 20:10:36 +0200

    Generated Caching-Header-Bug-Project

---
+++ src/test/java/com/example/cachingheaderbug/CachingHeaderbugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachingheaderbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingHeaderbugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
